0f8608e1fe18d3ff96672c57361b1f7db3dc6868
Jonathan O'Connor <JohnPierreOConnor@gmail.com>
2021-01-22 21:31:43 -0500

    added page to show score and input name upon completion of quiz

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,8 +5,11 @@ var questionList = { 1: "Commonly used data types DO NOT include:", 2: "The cond
 var answerList = { 1: ["strings", "booleans", "alerts", "numbers"], 2: ["quotes", "curly brackets", "parentheses", "square brackets"], 3: ["numbers and strings", "other arrays", "booleans", "all of the above"], 4: ["commas", "curly brackets", "quotes", "parentheses"], 5: ["Javascript", "terminal/bash", "for loops", "console.log"] }
 var answerKey = { 1: "alerts", 2: "parentheses", 3: "all of the above", 4: "quotes", 5: "console.log" }
 var questionNumber = 1
-var currentAnswer = ""
-var currentQuestion = ""
+var bodyContent = document.getElementById("body-content")
+
+if (localStorage.highScores) {
+    highScores = localStorage.highScores
+}
 
 function startQuiz() {
 
@@ -30,22 +33,54 @@ function updateTimer() {
 
 function endQuiz() {
     clearInterval(runTimer)
+    // build completion page
+    bodyContent.innerHTML = ""
 
-    // take to highscore page
+    //build all done message
+    var completionMessage = document.createElement("h1")
+    completionMessage.innerText = "All Done!"
+    bodyContent.appendChild(completionMessage)
+
+    //build score message
+    var scoreMessage = document.createElement("p")
+    scoreMessage.innerText = "Your final score is " + `${timer}`
+    bodyContent.appendChild(scoreMessage)
+
+    //build input to submit name which will be associated with score
+    var nameForm = document.createElement("form")
+    nameForm.id= "name-form"
+    bodyContent.appendChild(nameForm)
+    var nameLabel= document.createElement("label")
+    nameLabel.innerText= "Enter Initials"
+    document.getElementById("name-form").appendChild(nameLabel)
+    var nameInput = document.createElement("input")
+    nameInput.type = "text"
+    nameInput.id = "name-input"
+    document.getElementById("name-form").appendChild(nameInput)
+    var nameSubmit = document.createElement("button")
+    nameSubmit.innerText= "Submit"
+    nameSubmit.setAttribute("onClick", "addToHighscores()")
+    document.getElementById("name-form").appendChild(nameSubmit)
+}
+
+function addToHighscores () {
+
 }
 
 function buildQuestion() {
-    document.getElementById("div1").children[0].remove()
+    bodyContent.children[0].remove()
     var question = document.createElement("h1");
     question.id = "question"
     var questionText = questionList[1]
     var text = document.createTextNode(questionText)
     question.appendChild(text)
-    var contentBody = document.getElementById("div1")
-    contentBody.appendChild(question)
+    bodyContent.appendChild(question)
 }
 
 function buildAnswers() {
+    var buttonArea = document.createElement("div")
+    buttonArea.id = "answers"
+    bodyContent.appendChild(buttonArea)
     for (var i = 0; i < 4; i++) {
         var button = document.createElement("button")
         var answerText = answerList[1][i]
@@ -53,18 +88,19 @@ function buildAnswers() {
         button.appendChild(text)
         button.id = "answer-" + `${i}`
         button.setAttribute("onClick", "checkAnswer('" + button.id + "')")
-        var contentBody = document.getElementById("div1")
-        contentBody.appendChild(button)
+        buttonArea.appendChild(button)
+
     }
 
 }
 
 function checkAnswer(id) {
+    // reduce time if answer was wrong
     if (document.getElementById(id).innerHTML !== answerKey[questionNumber]) {
         timer -= 9
         updateTimer()
     }
-    // build next question
+    // build next question and corresponding answers
     questionNumber += 1
     if (questionNumber <= Object.keys(questionList).length) {
         document.getElementById("question").innerHTML = questionList[questionNumber]
