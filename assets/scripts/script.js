31a87950802fe4c03c9c840fb7a94bef36911faf
Jonathan O'Connor <JohnPierreOConnor@gmail.com>
2021-01-22 19:55:04 -0500

    added functions to handle going to next question

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,12 @@
 var highScores = {}
 var timer = 75
+var runTimer = ""
+var questionList = { 1: "Commonly used data types DO NOT include:", 2: "The condition in an if/else statement is enclosed within _________", 3: "Arrays in JavaScript can be used to store _______", 4: "String values must be enclosed within ______ when being assigned to variables", 5: "A very useful tool used during development and debugging for printing content to the debugger is:" }
+var answerList = { 1: ["strings", "booleans", "alerts", "numbers"], 2: ["quotes", "curly brackets", "parentheses", "square brackets"], 3: ["numbers and strings", "other arrays", "booleans", "all of the above"], 4: ["commas", "curly brackets", "quotes", "parentheses"], 5: ["Javascript", "terminal/bash", "for loops", "console.log"] }
+var answerKey = { 1: "alerts", 2: "parentheses", 3: "all of the above", 4: "quotes", 5: "console.log" }
+var questionNumber = 1
+var currentAnswer = ""
+var currentQuestion = ""
 
 function startQuiz() {
 
@@ -8,11 +15,12 @@ function startQuiz() {
 
     //start timer
     document.getElementById("time-count").innerHTML = `${timer}`
-    setInterval(updateTimer, 1000)
+    runTimer = setInterval(updateTimer, 1000)
 }
 
 function updateTimer() {
-    if (timer === 0) {
+    if (timer <= 0) {
+        document.getElementById("time-count").innerHTML = '0'
         endQuiz()
     } else {
         timer--
@@ -22,13 +30,16 @@ function updateTimer() {
 
 function endQuiz() {
     clearInterval(runTimer)
+
     // take to highscore page
 }
 
 function buildQuestion() {
     document.getElementById("div1").children[0].remove()
     var question = document.createElement("h1");
-    var text = document.createTextNode("Question 1")
+    question.id = "question"
+    var questionText = questionList[1]
+    var text = document.createTextNode(questionText)
     question.appendChild(text)
     var contentBody = document.getElementById("div1")
     contentBody.appendChild(question)
@@ -37,7 +48,8 @@ function buildQuestion() {
 function buildAnswers() {
     for (var i = 0; i < 4; i++) {
         var button = document.createElement("button")
-        var text = document.createTextNode("answer")
+        var answerText = answerList[1][i]
+        var text = document.createTextNode(answerText)
         button.appendChild(text)
         button.id = "answer-" + `${i}`
         button.setAttribute("onClick", "checkAnswer('" + button.id + "')")
@@ -48,5 +60,20 @@ function buildAnswers() {
 }
 
 function checkAnswer(id) {
-    console.log(id)
+    if (document.getElementById(id).innerHTML !== answerKey[questionNumber]) {
+        timer -= 9
+        updateTimer()
+    }
+    // build next question
+    questionNumber += 1
+    if (questionNumber <= Object.keys(questionList).length) {
+        document.getElementById("question").innerHTML = questionList[questionNumber]
+        for (var i = 0; i < 4; i++) {
+            document.getElementById("answer-" + `${[i]}`).innerHTML = answerList[questionNumber][i]
+        }
+    }
+    else {
+        endQuiz()
+    }
+
 }
